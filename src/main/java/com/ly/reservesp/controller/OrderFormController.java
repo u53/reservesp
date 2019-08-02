367324164e3aab9cb7cfc22a699d5a7704c36d6c
850681834 <850681834@qq.com>
2019-08-03 02:30:24 +0800

    订单表更新

--- src/main/java/com/ly/reservesp/controller/OrderFormController.java
+++ src/main/java/com/ly/reservesp/controller/OrderFormController.java
@@ -2,6 +2,7 @@ package com.ly.reservesp.controller;
 
 
 import com.ly.reservesp.entity.OrderForm;
+import com.ly.reservesp.entity.Reservations;
 import com.ly.reservesp.service.OrderFormService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -21,8 +22,10 @@ public class OrderFormController {
     public List<OrderForm> findOrderFormInfo(OrderForm orderForm){
 
         //订单表赋值有问题
-        //orderForm.getReservations().setReserveId(1);
-
+        Reservations reservations = new Reservations();
+        reservations.setReserveId(1);
+        orderForm.setReservations(reservations);
+        orderForm.setAmount(98.0);
         return ofs.findOrderFormInfo(orderForm);
     }
 
