11635a33d9c6a521bd35d27e7c9802acaa99d794
u53 <1065378079@qq.com>
2019-08-02 19:33:30 +0800

    预约优化更新

--- src/main/java/com/ly/reservesp/controller/FoodInformationController.java
+++ src/main/java/com/ly/reservesp/controller/FoodInformationController.java
@@ -12,7 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController("/FoodInformation")
+@RestController
+@RequestMapping("/FoodInformation")
 public class FoodInformationController {
 
     @Autowired
